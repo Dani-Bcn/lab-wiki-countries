93b925b92a18db81e8dde492e808bbefb2c5039d
unknown <nneodani@gmail.com>
2022-08-27 22:15:27 +0200

    initial first commit

---
+++ src/components/CountryDetails.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+export default function CountryDetails() {
+  return (
+    <div>CountryDetails</div>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='containerNav'>
+        <h1>LAB-WikiCountries</h1>
+    </div>
+  )
+}
